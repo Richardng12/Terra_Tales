06dd6e3709b9d75b7fa5b9c96baaebea37bc6c03
daltonlim <daltonlim123@gmail.com>
2019-10-01 17:47:32 +1300

    Timer working

--- Assets/Scripts/TimerFillBar.cs
+++ Assets/Scripts/TimerFillBar.cs
@@ -5,24 +5,21 @@ using UnityEngine.UI;
 
 public class TimerFillBar : MonoBehaviour
 {
-    public float secondsToCountDown;
-    private float secondsLeft;
-    public Text text;
+    private Slider slider;
+    private float currentValue;
 
-    // Unity UI References
-    public Slider slider;
-    public Text displayText;
-
-    float currentValue = 0f;
     // Start is called before the first frame update
     void Start()
     {
-        currentValue += 0.1f;
+        slider = GetComponent<Slider>();
+        currentValue = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
+        currentValue += 0.1f;
+
         slider.value = currentValue;
     }
 }
