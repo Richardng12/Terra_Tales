60f214bd57301809d9b16d4674f986d8b20dd026
daltonlim <daltonlim123@gmail.com>
2019-10-01 23:03:24 +1300

    Add max energy detection

---
+++ Assets/Scripts/Building.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Building : MonoBehaviour
+{
+    public Rigidbody2D switch0;
+    public Rigidbody2D switch1;
+    public Rigidbody2D switch2;
+    public Text text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (switch0.gameObject.GetComponent<Renderer>().material.color == switch1.gameObject.GetComponent<Renderer>().material.color && 
+            switch1.gameObject.GetComponent<Renderer>().material.color == switch2.gameObject.GetComponent<Renderer>().material.color &&
+            switch0.gameObject.GetComponent<Renderer>().material.color == new Color(249, 166, 2))
+        {
+            text.text = "Oh no energy max reached";
+            //Decrease energy. Turn on aurora of POWA
+        }
+    }
+}
---
+++ Assets/Scripts/Window.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//https://www.youtube.com/watch?v=yFKg8qVclBk
+public class Window: MonoBehaviour
+{
+
+    // Reference to Sprite Renderer component
+    private Renderer rend;
+
+    public Rigidbody2D switch0;
+
+    // Use this for initialization
+    private void Start()
+    {
+        rend = GetComponent<Renderer>();
+        ChangeState();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+            ChangeState();
+    }
+
+    private void ChangeState()
+    {
+            rend.material.color = switch0.gameObject.GetComponent<Renderer>().material.color;
+    }
+
+}
