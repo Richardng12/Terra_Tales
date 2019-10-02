6af4bce98b475a7162bc2d16e1b79a6b1a72c904
Richard Ng <richardng1998@gmail.com>
2019-10-02 17:46:52 +1300

    Text shows above player to indicate you can interact with NPC
    Text shows above player to indicate you can interact with NPC

---
+++ Assets/Scripts/ForestNPCInteraction.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ForestNPCInteraction : MonoBehaviour
+{
+    public bool Interactable = false;
+
+    public Text showText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        showText.text = "";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("f") && Interactable)
+        {
+            Debug.Log("Hello");
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D Collision)
+    {
+        if (Collision.gameObject.tag.Equals("Player"))
+        {
+            Debug.Log(Interactable);
+            showText.gameObject.SetActive(true);
+            Interactable = true;
+            showText.text = "Press E";
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D Collision)
+    {
+        if (Collision.gameObject.tag.Equals("Player"))
+        {
+            Debug.Log(Interactable);
+            showText.gameObject.SetActive(true);
+            Interactable = false;
+            showText.text = "";
+        }
+    }
+}
