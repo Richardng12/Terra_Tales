51090a7a75251845cabcc90c9c7d5cac4fb2ad37
Bryan <bryanang1998@gmail.com>
2019-10-02 17:35:29 +1300

    Slightly added progress bar

--- Assets/Scripts/Seedling.cs
+++ Assets/Scripts/Seedling.cs
@@ -4,40 +4,48 @@ using UnityEngine;
 
 public class Seedling : MonoBehaviour
 {
-
     public bool Interactable = false;
     public float CurrentProgress;
-    public  readonly float MaxProgress = 5;
+    public readonly float MaxProgress = 5;
+
     // Start is called before the first frame update
     void Start()
     {
         CurrentProgress = 0;
+        transform.Find("ProgressBar").localScale = new Vector3(0, 1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey("f") && Interactable)
+        if (Input.GetKey("e") && Interactable && CurrentProgress > 5)
+        {
+            Interactable = false;
+            transform.Find("ProgressBar").localScale = new Vector3(0, 1);
+
+        }
+        if (Input.GetKey("e") && Interactable)
         {
             CurrentProgress += Time.deltaTime;
             Debug.Log(CurrentProgress);
+            float progress = CurrentProgress / MaxProgress;
+            Debug.Log("progress" + progress);
+            transform.Find("ProgressBar").localScale = new Vector3(1 - progress, 1);
         }
-        if(Input.GetKey("f") && Interactable && CurrentProgress == 5)
-        {
-            Interactable = false;
-        }
-        if (Input.GetKeyUp("f") && CurrentProgress < MaxProgress )
+        if (Input.GetKeyUp("e") && CurrentProgress < MaxProgress)
         {
             CurrentProgress = 0;
+            transform.Find("ProgressBar").localScale = new Vector3(0, 1);
         }
 
     }
 
     private void OnTriggerEnter2D(Collider2D Collision)
     {
-        if (Collision.gameObject.tag.Equals("Player"))
+        if (Collision.gameObject.tag.Equals("Player") && CurrentProgress <= MaxProgress)
         {
             Debug.Log(Interactable);
+            transform.Find("ProgressBar").localScale = new Vector3(1, 1);
             Interactable = true;
         }
     }
@@ -47,6 +55,7 @@ public class Seedling : MonoBehaviour
         if (Collision.gameObject.tag.Equals("Player"))
         {
             Debug.Log(Interactable);
+            transform.Find("ProgressBar").localScale = new Vector3(0, 1);
             Interactable = false;
             CurrentProgress = 0;
         }
