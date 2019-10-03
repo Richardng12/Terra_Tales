566654f103d87649e8edc6a1cad698484f52fb51
Bryan <bryanang1998@gmail.com>
2019-10-04 11:11:33 +1300

    Finished popup text with a fadeout

--- Assets/Scripts/TreeCounter.cs
+++ Assets/Scripts/TreeCounter.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TreeCounter : MonoBehaviour
 {
+    public int treesPlanted;
+
+    public Text treeText;
+
+    public float fadeOutTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+        treesPlanted = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void updateAndDisplayTreeCounter()
     {
-        
+        treesPlanted++;
+        treeText.text = "Planted " + treesPlanted + "/10 Trees";
+        StartCoroutine(TextFadeOutRoutine());
+    }
+
+    private IEnumerator TextFadeOutRoutine()
+    {
+        Color color = treeText.color;
+
+        for (float t = 0.01f; t < fadeOutTime; t += Time.deltaTime)
+        {
+            treeText.color = Color.Lerp(color, Color.clear, Mathf.Min(1, t / fadeOutTime));
+
+            yield return null;
+        }
     }
 }
