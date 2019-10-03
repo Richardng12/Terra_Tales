152779df68437577e9f0a1ef8e7f1ab841f1f092
tomatopaste202 <thomaszhu.nz@gmail.com>
2019-10-03 15:46:29 +1300

    Enemy prefab
    Created an enemy which chases the player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,56 +4,88 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    /**
 
-    public float speed;
-
-    //The object the enemy is chasing
-    //Transform is the position vector
     private Transform playerPos;
 
-    //reference the player object
     private CharacterController player;
 
-    
-    //Variables to give enemy velocity
-    private Vector moveVelocity;
-    private Vector moveInput;
-    private Vector pos;
-    private Vector tempMove;
-    private Vector oppMove;
-    
-
-    //initialize the enemy body
     private Rigidbody2D rb;
+
+
+    //Vectors
+    private Vector2 moveVelocity;
+    private Vector2 moveInput;
+    private Vector2 pos;
+    private Vector2 tempMove;
+    private Vector2 oppMove;
 
     // Start is called before the first frame update
     void Start()
     {
-
-        
-        //Find the player which has the tag of Player and grab player script
         player = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>();
-        //Find the position of the "target" which has the tag of Player
-        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
-        
 
-        rb = GetComponent<Rigidbody2D>();
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+
+        rb.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        
-
+        /**
         pos = new Vector2(playerPos.position.x, playerPos.position.y);
         oppMove = rb.position - pos;
         moveInput = pos - rb.position;
-        moveVelocity = moveInput.normalized * speed;
-        colCount = 0;
-        time = 100;
+        moveVelocity = moveInput.normalized * 4;
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
     
+
+        moveVelocity = new Vector2(5, 5);
+        rb.MovePosition(rb.position + moveVelocity *Time.fixedDeltaTime);
+       
+        
+    }
+
+    */
+
+
+    private Transform playerPos;
+
+    private CharacterController player;
+
+    private Rigidbody2D rb;
+
+    private Vector2 velocity;
+
+
+    private Vector2 moveVelocity;
+    private Vector2 moveInput;
+    private Vector2 pos;
+    private Vector2 tempMove;
+    private Vector2 oppMove;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<CharacterController>();
+
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform;
+
+        //velocity = new Vector2(5f, 5f);
+        rb = gameObject.GetComponent<Rigidbody2D>();
+
+
+    }
+
+    void Update()
+    {
+        pos = new Vector2(playerPos.position.x, playerPos.position.y);
+        oppMove = rb.position - pos;
+        moveInput = pos - rb.position;
+        moveVelocity = moveInput.normalized * 4;
 
         rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
     }
+
+
 }
