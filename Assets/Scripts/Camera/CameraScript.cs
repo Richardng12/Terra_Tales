b530c25d0e7f553532039ba9e8b1f458879677a8
Hong <hongye1007@gmail.com>
2019-10-03 14:05:38 +1300

    Changed Camera script to contain a player

--- Assets/Scripts/Camera/CameraScript.cs
+++ Assets/Scripts/Camera/CameraScript.cs
@@ -6,19 +6,18 @@ public class CameraScript : MonoBehaviour
 {
     public float cameraDistOffset = 10;
     private Camera mainCamera;
-    private GameObject player;
+    public Transform player;
 
     // Use this for initialization
     void Start()
     {
         mainCamera = GetComponent<Camera>();
-        player = GameObject.Find("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 playerInfo = player.transform.transform.position;
+        Vector3 playerInfo = player.transform.position;
         mainCamera.transform.position = new Vector3(playerInfo.x, playerInfo.y, playerInfo.z - cameraDistOffset);
     }
 }
