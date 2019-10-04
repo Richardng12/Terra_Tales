1fc058e65835e18964d62f0ef8eb35b738f32dee
Hong <hongye1007@gmail.com>
2019-10-04 13:06:41 +1300

    added TrashScript and SpawnerSCript schange

---
+++ Assets/Scripts/Items/TrashScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class TrashScript : AbstractSpawnableObject
+{
+    public SpawnerScript spawner;
+    private int spawnLocation;
+
+    private void Start()
+    {
+        spawner = GameObject.FindGameObjectWithTag("ItemSpawner").GetComponent<SpawnerScript>();
+    }
+
+    public void SetLocation(int location) {
+
+        spawnLocation = location;
+    }
+
+    public void OnDestroy()
+    {
+        spawner.getSpawnedObjects()[spawnLocation] = null;
+        spawner.currentSpawnDelay = 0;
+    }
+}
