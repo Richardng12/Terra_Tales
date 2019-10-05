557632ca4f22ca15d48a825fd531f236eb634626
Dalton Lim <39323194+daltonlim@users.noreply.github.com>
2019-10-05 21:37:45 +1300

    All levels accesible from mian screen

---
+++ Assets/Menu/MenuScreenAssets/Scripts/LoadScene3.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadScene3 : MonoBehaviour
+{
+    void Start () {
+		Button btn = GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+        SceneManager.LoadSceneAsync(3);
+
+		Debug.Log ("You have clicked the button!");
+	}
+
+
+}
