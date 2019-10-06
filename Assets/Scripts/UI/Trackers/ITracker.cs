ec54cb6ff1e86855eae98a53aca585b8d377f3b6
Hong <hongye1007@gmail.com>
2019-10-06 16:35:14 +1300

    Added a tracker to know when task is completed

---
+++ Assets/Scripts/UI/Trackers/ITracker.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ interface ITracker<T>
+{
+    void UpdateAndDisplayTaskCounter(T i);
+
+    IEnumerator TextFadeOutRoutine();
+}
+    
