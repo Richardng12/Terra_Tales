442709901be88957cfc068e7f2449a3c9cfabdee
Bryan <bryanang1998@gmail.com>
2019-10-06 16:35:29 +1300

    Added base scoring script

---
+++ Assets/Scripts/UI/Scoring.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scoring : MonoBehaviour
+{
+    // Start is called before the first frame update
+    static public int forestScore = 0;
+
+    static public int oceanScore = 0;
+
+    static public int cityScore = 0;
+
+    public int overallScore;
+    public void CalculateStageScore(string stage)
+    {
+        int currentScore = ScoreCalculation();
+        switch (stage)
+        {
+            case "Forest":
+                forestScore = Math.Max(currentScore, forestScore);
+                break;
+            case "Ocean":
+                oceanScore = Math.Max(currentScore, oceanScore);
+                break;
+            case "City":
+                cityScore = Math.Max(currentScore, cityScore);
+                break;
+            default:
+                overallScore = forestScore + oceanScore + cityScore;
+        }
+    }
+
+
+    private int ScoreCalculation()
+    {
+
+    }
+}
