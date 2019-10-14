96fe2687ff865b421492e0cea0645d403d08b889
Richard Ng <richardng1998@gmail.com>
2019-10-14 14:18:01 +1300

    Added fade in/out

--- Assets/Scripts/Timeline/FirstStartTimeLine.cs
+++ Assets/Scripts/Timeline/FirstStartTimeLine.cs
@@ -9,13 +9,19 @@ public class FirstStartTimeLine : MonoBehaviour
     public GameObject firstStartTimeLine;
     public GameObject player;
     public GameObject[] objects;
+
+    public GameObject blackFade;
+
+
     // Start is called before the first frame update
     void Start()
     {
         //Cancel player movement during cutscene
         player = GameObject.FindWithTag("Player");
         player.GetComponent<CharacterController>().enabled = false;
-        
+        blackFade = GameObject.FindGameObjectWithTag("BlackFade");
+
+        StartCoroutine(delayedPlayback());
     }
 
     // Update is called once per frame
@@ -29,10 +35,16 @@ public class FirstStartTimeLine : MonoBehaviour
 		//When initial forest cutscene is finished, set the component to non-active and enable the player movement
         if (timeline == aDirector)
         {
+            blackFade.SetActive(false);
             firstStartTimeLine.SetActive(false);
             player.GetComponent<CharacterController>().enabled = true;
-
         }
+
+    }
+
+    IEnumerator delayedPlayback(){
+        yield return new WaitForSeconds(3.5f);
+         blackFade.GetComponent<DialogueFade>().FadeToDialogue();
     }
 
 	//Lifecycle methods that get called once cutscene is finished
