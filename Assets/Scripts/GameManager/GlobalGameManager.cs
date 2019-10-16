c3ef24d532c6a036cf2d16c9c9fe60a88eda2bdb
daltonlim <daltonlim123@gmail.com>
2019-10-16 14:54:15 +1300

    Add global game manger. Fix up tut water gun

---
+++ Assets/Scripts/GameManager/GlobalGameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalGameManager : MonoBehaviour
+{
+
+    public static GlobalGameManager instance;
+    public bool firstPlay;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            // Dont destroy this object when a new scene is loaded
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            if (instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
