c1be52fbdd600195e6e6ab7ee873370998314fe6
Hong <hongye1007@gmail.com>
2019-10-18 16:51:54 +1300

    Added loading screen

---
+++ Assets/Scripts/GameManager/LevelLoader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+//https://www.youtube.com/watch?v=YMj2qPq9CP8
+public class LevelLoader : MonoBehaviour
+{
+    public GameObject loadingScreen;
+    public Slider slider;
+
+   public void LoadLevel(int sceneIndex)
+    {
+        loadingScreen.SetActive(true);
+        StartCoroutine(LoadAsynchrously(sceneIndex));
+    }
+
+    IEnumerator LoadAsynchrously(int sceneIndex)
+    {
+        AsyncOperation loader = SceneManager.LoadSceneAsync(sceneIndex);
+        while (!loader.isDone)
+        {
+            float progress = Mathf.Clamp01(loader.progress / 0.9f);
+            slider.value = progress;
+            yield return null;
+        }
+        loadingScreen.SetActive(false);
+        slider.value = 0;
+
+    }
+}
