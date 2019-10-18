da0797690feb571b3b86c12624bff102cc4b930c
Bryan <bryanang1998@gmail.com>
2019-10-18 17:14:45 +1300

    added difficulty settings for forest

--- Assets/Scripts/GameManager/GlobalGameManager.cs
+++ Assets/Scripts/GameManager/GlobalGameManager.cs
@@ -7,6 +7,8 @@ public class GlobalGameManager : MonoBehaviour
 
     public static GlobalGameManager instance;
     public bool firstPlay = true;
+    private ForestLevelProperties[] forestProperties;
+    public ForestLevelProperties chosenForestLevel;
 
 
     public IEnumerator delayedSet(bool b)
@@ -14,11 +16,45 @@ public class GlobalGameManager : MonoBehaviour
         yield return new WaitForSeconds(3);
         firstPlay = b;
     }
-    // Start is called before the first frame update
-    void Start()
+    private void OnEnable()
     {
-
+        Publisher.StartListening("ForestLevelEasy", SetForestLevelEasy);
+        Publisher.StartListening("ForestLevelMedium", SetForestLevelMedium);
+        Publisher.StartListening("ForestLevelHard", SetForestLevelHard);
     }
+
+    private void OnDisable()
+    {
+        Publisher.StopListening("ForestLevelEasy", SetForestLevelEasy);
+        Publisher.StopListening("ForestLevelMedium", SetForestLevelMedium);
+        Publisher.StopListening("ForestLevelHard", SetForestLevelHard);
+    }
+
+    private void SetForestLevelEasy()
+    {
+        SetForestProperties(0);
+    }
+
+    private void SetForestLevelMedium()
+    {
+        SetForestProperties(1);
+    }
+
+    private void SetForestLevelHard()
+    {
+        SetForestProperties(2);
+    }
+
+    private void SetForestProperties(int i)
+    {
+         chosenForestLevel = forestProperties[i];
+    }
+
+    private void Start()
+    {
+        InitLevels();
+    }
+
     void Awake()
     {
         if (instance == null)
@@ -35,9 +71,32 @@ public class GlobalGameManager : MonoBehaviour
             }
         }
     }
-    // Update is called once per frame
-    void Update()
-    {
 
+    private void InitLevels()
+    {
+        forestProperties = new ForestLevelProperties[3]
+        {
+            new ForestLevelProperties(360, 6, 4),
+            new ForestLevelProperties(300, 4, 6),
+            new ForestLevelProperties(240, 2, 8)
+        };
+    }
+}
+
+public abstract class LevelProperties
+{
+    public int time;
+    public int spawnRate;
+}
+
+public class ForestLevelProperties : LevelProperties
+{
+    public int treesToPlant;
+
+    public ForestLevelProperties(int levelTime, int levelSpawnRate, int levelTreesToPlant)
+    {
+        time = levelTime;
+        spawnRate = levelSpawnRate;
+        treesToPlant = levelTreesToPlant;
     }
 }
---
+++ Assets/Scripts/GameManager/SetForestDifficulty.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetForestDifficulty : MonoBehaviour
+{
+    public GameObject spawnerObject;
+
+    public GameObject timerObject;
+
+    public GameObject treeTracker;
+
+
+    void Start()
+    {
+        spawnerObject.GetComponent<SpawnerScript>().spawnDelay = GlobalGameManager.instance.chosenForestLevel.spawnRate;
+        timerObject.GetComponent<Timer>().time = GlobalGameManager.instance.chosenForestLevel.time;
+        treeTracker.GetComponent<ForestTracker>().treesToPlant = GlobalGameManager.instance.chosenForestLevel.treesToPlant;
+    }
+}
