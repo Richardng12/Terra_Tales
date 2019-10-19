c08c6e3b77bf004fec2d40410ff93d4833d6ebda
Hong <hongye1007@gmail.com>
2019-10-20 11:54:39 +1300

    Added HUDTutorial prefab and tips on loading screen

--- Assets/HouseTrigger.cs
+++ Assets/HouseTrigger.cs
@@ -6,24 +6,12 @@ using UnityEngine.SceneManagement;
 public class HouseTrigger : MonoBehaviour
 {
     public LevelLoader levelLoader;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
      private void OnTriggerEnter2D(Collider2D Collision)
     {
         // If collision is detected then go back to main menu
         if (Collision.gameObject.tag.Equals("Player"))
         {
-
              levelLoader.LoadLevel(2);
         }
     }
