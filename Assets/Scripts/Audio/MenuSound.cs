3f107e1522956c39d4854e3ea53e3d25035e338a
Hong <hongye1007@gmail.com>
2019-10-21 16:05:49 +1300

    Fixed all glitches

---
+++ Assets/Scripts/Audio/MenuSound.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuSound : MonoBehaviour
+{
+    string hoverSound = "HoverSound";
+    string clickSound = "ClickSound";
+    string MainMenuSound = "MainMenu";
+
+
+    public void OnMouseOver()
+    {
+        AudioManager.instance.Play(hoverSound);
+    }
+    public void OnMouseClick()
+    {
+        AudioManager.instance.Play(clickSound);
+    }
+}
