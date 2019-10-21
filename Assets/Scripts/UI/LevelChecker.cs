0921856537cd16bb05b0f11795c4aef7adb08c6c
Bryan <bryanang1998@gmail.com>
2019-10-21 13:34:46 +1300

    added lock and difficulty selection to levels

--- Assets/Scripts/UI/LevelChecker.cs
+++ Assets/Scripts/UI/LevelChecker.cs
@@ -7,17 +7,52 @@ using UnityEngine.UI;
 public class LevelChecker : MonoBehaviour
 {
     public Button oceanButton;
+    public Image oceanLockImage;
 
     public Button forestButton;
+    public Image forestLockImage;
+
     private void OnEnable()
     {
         if(GlobalGameManager.instance.forestUnlocked == false)
         {
-            forestButton.interactable = false;
+            DisableButton(forestButton);
+            forestLockImage.enabled = true;
+        }
+        else
+        {
+            EnableButton(forestButton);
+            forestLockImage.enabled = false;
         }
         if(GlobalGameManager.instance.oceanUnlocked == false)
         {
-            oceanButton.interactable = false;
+            DisableButton(oceanButton);
+            oceanLockImage.enabled = true;
+        }
+        else
+        {
+            EnableButton(oceanButton);
+            oceanLockImage.enabled = false;
+        }
+    }
+
+
+
+    private void DisableButton(Button button)
+    {
+        button.interactable = false;
+        foreach (var variable in button.GetComponents<ChangeColorOnHover>())
+        {
+            variable.enabled = false;
+        }
+    }
+
+    private void EnableButton(Button button)
+    {
+        button.interactable = true;
+        foreach (var variable in button.GetComponents<ChangeColorOnHover>())
+        {
+            variable.enabled = true;
         }
     }
 }
